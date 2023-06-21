5342202d4db428272dbeba5df12d3aedd92cb9a7
Wyclife Kimutai <kimutaiwyclife6@gmail.com>
2023-06-21 13:47:31 -0700

    putchar correstion

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,21 +7,19 @@
 #include "main.h"
 
 /**
- * main - Prints "Putchar" followed by a new line.
- *
+ * main - print _putchar
  * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
