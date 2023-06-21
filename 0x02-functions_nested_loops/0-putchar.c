7d4cbb674bba97abe0501996faf368e9503a7ecb
wyclife <81298471+kbett035@users.noreply.github.com>
2023-06-21 22:46:42 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Wyclife Kimutai
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "Putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
