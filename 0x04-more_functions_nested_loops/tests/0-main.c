d0b240d72fe49da3bfab2e835dfdf1905a02cb41
wyclife <81298471+kbett035@users.noreply.github.com>
2023-06-23 11:02:31 +0300

    Update 0-main.c

--- 0x04-more_functions_nested_loops/tests/0-main.c
+++ 0x04-more_functions_nested_loops/tests/0-main.c
@@ -11,8 +11,8 @@ int main(void)
 	char c;
 
 	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isuper(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isuper(c));
 	return (0);
 }
