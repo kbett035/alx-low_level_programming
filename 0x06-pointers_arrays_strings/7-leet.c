8215dd2813eb54fd4b83ded20f23d0e3d2b78146
Wyclife Kimutai <kimutaiwyclife6@gmail.com>
2023-06-29 09:50:48 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,26 @@
-i#include "main.h"
-/**
- * leet - encode into 1337speak
- * @n: input value
- * Return: n value
- */
-char *leet(char *n)
-{
-	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+#include <stdlib.h>
 
-	for (i = 0; n[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
-	}
-	return (n);
+char *leet(char *str) {
+    char *ptr = str;
+    char *leetStr = str;
+    char leetMap[10] = {'o', 'l', 'e', 'a', 't', 'O', 'L', 'E', 'A', 'T'};
+    char leetDigits[10] = {'0', '1', '3', '4', '7', '0', '1', '3', '4', '7'};
+    int i, j;
+
+    for (i = 0; *ptr != '\0'; i++) {
+        for (j = 0; j < 10; j++) {
+            if (*ptr == leetMap[j]) {
+                *leetStr = leetDigits[j];
+                break;
+            }
+        }
+        if (j == 10) {
+            *leetStr = *ptr;
+        }
+        ptr++;
+        leetStr++;
+    }
+
+    *leetStr = '\0';
+    return str;
 }
